a3f74df7cd038f3422e87b3406d32382429fee83
Corey Oordt <coreyoordt@gmail.com>
2012-03-23 07:34:21 -0400

    Version bump to 0.5

--- elections/__init__.py
+++ elections/__init__.py
@@ -5,8 +5,8 @@ __version_info__ = {
     'major': 0,
     'minor': 5,
     'micro': 0,
-    'releaselevel': 'beta',
-    'serial': 6
+    'releaselevel': 'final',
+    'serial': 1
 }
 
 def get_version(short=False):
